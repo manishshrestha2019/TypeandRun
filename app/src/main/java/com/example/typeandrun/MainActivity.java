8f49d464a074a90a39917fb5a1cf030540f8ac08
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-10 16:06:17 +0545

    added input form validaton

--- app/src/main/java/com/example/typeandrun/MainActivity.java
+++ app/src/main/java/com/example/typeandrun/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.typeandrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,17 +31,53 @@ public class MainActivity extends AppCompatActivity {
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!vaidate()){
+                    return;
+                }
             String make=etMake.getText().toString();
             int year= Integer.parseInt(etYear.getText().toString());
             String color=etColor.getText().toString();
             float price=Float.parseFloat(etPrice.getText().toString());
             int enginesize=Integer.parseInt(etSize.getText().toString());
+            i++;
 
-            tvOutput.append("This is the vehicle no " +i+"\n" +"Made by "+make+ "\n"+ "Make year" +year+"\n"
+            tvOutput.append("\n This is the vehicle no " +i+"\n" +"Made by "+make+ "\n"+ "Make year " +year+"\n"
                     +"Color is "+ color+"\n"+ "Purchase Price  "+price+ "\n"+"Engine size is "+enginesize+" liter ");
             }
         });
 
 
+    }
+
+    public boolean vaidate(){
+        boolean isValid=true;
+
+        if(TextUtils.isEmpty(etMake.getText().toString())){
+            etMake.setError("Please Enter Manufacturer");
+            etMake.requestFocus();
+            isValid=false;
+        }else if(TextUtils.isEmpty(etColor.getText().toString())){
+            etColor.setError("Please Enter Color");
+            etColor.requestFocus();
+            isValid=false;
+        }
+        if(TextUtils.isEmpty(etSize.getText().toString())){
+            etSize.setError("Please Enter Engine size");
+            etSize.requestFocus();
+            isValid=false;
+        }
+        if(TextUtils.isEmpty(etPrice.getText().toString())){
+            etPrice.setError("Please Enter Price");
+            etPrice.requestFocus();
+            isValid=false;
+        }if(TextUtils.isEmpty(etYear.getText().toString())){
+            etYear.setError("Please Enter Year");
+            etYear.requestFocus();
+            isValid=false;
+        }if(TextUtils.isEmpty(etColor.getText().toString())){
+            etColor.setError("Please Enter color");
+            etColor.requestFocus();
+            isValid=false;
+        }return isValid;
     }
 }
